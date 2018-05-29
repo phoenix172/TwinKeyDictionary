3fdcce61aa4a081802cf9d8c80c170fb0eedfeb2
Ivan Yochev <vani172@gmail.com>
2018-05-29 14:58:09 +0300

    initial

---
+++ TwinKeyDictionary/TwinKeyDictionary.cs
@@ -0,0 +1,129 @@
+﻿namespace TwinKeyDictionary
+{
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+    /// <summary>
+    /// Represents a dictionary with two keys.
+    /// Records can be looked up either by using only the primary key, or by using both the primary and secondary keys.
+    /// If there are multiple records with the same primary key and no secondary key is specified, the first record with that primary key is returned.
+    /// </summary>
+    /// <typeparam name="TKeyPrimary">The type of the primary key.</typeparam>
+    /// <typeparam name="TKeySecondary">The type of the secondary key.</typeparam>
+    /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+    public class TwinKeyDictionary<TKeyPrimary,TKeySecondary,TValue> :  
+        Dictionary<Tuple<TKeyPrimary, TKeySecondary>, TValue>,
+        IDictionary<TKeyPrimary, TValue>
+    {
+        ICollection<TKeyPrimary> IDictionary<TKeyPrimary, TValue>.Keys => Keys.Select(x => x.Item1).Distinct().ToList();
+
+        ICollection<TValue> IDictionary<TKeyPrimary, TValue>.Values => Values;
+
+        public bool IsReadOnly => false;
+
+        public TValue this[TKeyPrimary primaryKey, TKeySecondary secondaryKey]
+        {
+            get { return base[Tuple.Create(primaryKey, secondaryKey)]; }
+            set { base[Tuple.Create(primaryKey, secondaryKey)] = value; }
+        }
+
+        public TValue this[TKeyPrimary primaryKey]
+        {
+            get { return base[GetKeyByPrimary(primaryKey)]; }
+            set { base[GetKeyByPrimary(primaryKey)] = value; }
+        }
+
+        public void Add(TKeyPrimary primaryKey, TKeySecondary secondaryKey, TValue value)
+        {
+            Add(Tuple.Create(primaryKey, secondaryKey), value);
+        }
+
+        public bool ContainsKey(TKeyPrimary primaryKey, TKeySecondary secondaryKey)
+        {
+            return ContainsKey(Tuple.Create(primaryKey, secondaryKey));
+        }
+
+        public bool ContainsKey(TKeyPrimary primaryKey)
+        {
+            return ContainsKey(primaryKey, out _);
+        }
+
+        private bool ContainsKey(TKeyPrimary primaryKey, out Tuple<TKeyPrimary, TKeySecondary> key)
+        {
+            key = GetKeyByPrimary(primaryKey);
+            return key != null;
+        }
+
+        public bool TryGetValue(TKeyPrimary primaryKey, out TValue value)
+        {
+            value = default(TValue);
+            if (ContainsKey(primaryKey, out var key))
+            {
+                value = this[key];
+                return true;
+            }
+            return false;
+        }
+
+        public bool TryGetValue(TKeyPrimary primaryKey, TKeySecondary secondaryKey, out TValue value)
+        {
+            return TryGetValue(Tuple.Create(primaryKey, secondaryKey), out value);
+        }
+
+        private Tuple<TKeyPrimary, TKeySecondary> GetKeyByPrimary(TKeyPrimary primaryKey)
+        {
+            return Keys.FirstOrDefault(x => x.Item1.Equals(primaryKey));
+        }
+
+        public bool Remove(TKeyPrimary primaryKey)
+        {
+            bool hasKey = ContainsKey(primaryKey, out var key);
+            if (!hasKey) return false;
+            return Remove(key);
+        }
+
+        public bool Remove(TKeyPrimary primaryKey, TKeySecondary secondaryKey)
+        {
+            return Remove(Tuple.Create(primaryKey, secondaryKey));
+        }
+
+        public void Add(TKeyPrimary key, TValue value)
+        {
+            Add(Tuple.Create(key, default(TKeySecondary)), value);
+        }
+
+        public void Add(KeyValuePair<TKeyPrimary, TValue> item)
+        {
+            Add(item.Key, item.Value);
+        }
+
+        public bool Contains(KeyValuePair<TKeyPrimary, TValue> item)
+        {
+            bool hasKey = ContainsKey(item.Key, out var key);
+            if (!hasKey) return false;
+            return this.Contains(new KeyValuePair<Tuple<TKeyPrimary, TKeySecondary>, TValue>(key, item.Value));
+        }
+
+        public void CopyTo(KeyValuePair<TKeyPrimary, TValue>[] array, int arrayIndex)
+        {
+            AsBaseEnumerable().ToList().CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(KeyValuePair<TKeyPrimary, TValue> item)
+        {
+            return Remove(item.Key);
+        }
+
+        IEnumerator<KeyValuePair<TKeyPrimary, TValue>> IEnumerable<KeyValuePair<TKeyPrimary, TValue>>.GetEnumerator()
+        {
+            return AsBaseEnumerable().GetEnumerator();
+        }
+
+        private IEnumerable<KeyValuePair<TKeyPrimary, TValue>> AsBaseEnumerable()
+        {
+            return (this as IEnumerable<KeyValuePair<Tuple<TKeyPrimary, TKeySecondary>, TValue>>)
+                .Select(x => new KeyValuePair<TKeyPrimary, TValue>(x.Key.Item1, x.Value));
+        }
+    }
+}
---
+++ TwinKeyDictionaryTests/TwinKeyDictionaryTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using TwinKeyDictionary;
+
+namespace TwinKeyDictionaryTests
+{
+    [TestFixture]
+    public class TwinKeyDictionaryTest
+    {
+        private TwinKeyDictionary<int, string, string> _dictionary;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dictionary = new TwinKeyDictionary<int, string, string> 
+                {
+                    {1, "asd", "gosho"}, 
+                    {1, "bsd", "pesho"},
+                    {2, "asd", "sasho"},
+                    {3, "aaa", "penka"}
+                };
+        }
+
+        [Test]
+        public void Add_WithPrimaryKey_ContainsRecordWithDefaultSecondaryKey()
+        {
+            _dictionary.Add(1, "stamat");
+
+            Assert.That(_dictionary.Contains(new KeyValuePair<Tuple<int,string>, string>
+                (Tuple.Create(1, default(string)), "stamat")));
+        }
+
+        [Test]
+        public void TryGetValue_ByPrimaryKey_ReturnsFirstRecordWithSuchKey()
+        {
+            bool success = _dictionary.TryGetValue(1, out var value);
+            
+            Assert.That(success);
+            Assert.That(value, Is.EqualTo("gosho"));
+        }
+
+        [Test]
+        public void Indexer_ByPrimaryKey_ReturnsFirstRecordWithSuchKey()
+        {
+            var value = _dictionary[1];
+            
+            Assert.That(value, Is.EqualTo("gosho"));
+        }
+
+        [Test]
+        public void Contains_ExistingPrimaryKeyAndValue_ReturnsTrue()
+        {
+            bool contains = _dictionary.Contains(new KeyValuePair<int, string>(1, "gosho"));
+            
+            Assert.That(contains);
+        }
+
+        [Test]
+        public void Remove_ByPrimaryKey_RemoveFirstRecordWithSuchKey()
+        {
+            _dictionary.Remove(1);
+            
+            Assert.That(_dictionary, Does.Not.ContainValue("gosho"));
+        }
+
+        [Test]
+        public void TryGetValue_ByPrimaryAndSecondaryKey_ReturnsCorrectRecord()
+        {
+            bool success = _dictionary.TryGetValue(Tuple.Create(1, "bsd"), out string value);
+            
+            Assert.That(success);
+            Assert.That(value, Is.EqualTo("pesho"));
+        }
+
+        [Test]
+        public void Indexer_ByPrimaryAndSecondaryKey_ReturnsCorrectRecord()
+        {
+            var value = _dictionary[1, "bsd"];
+            
+            Assert.That(value, Is.EqualTo("pesho"));
+        }
+    }
+}
